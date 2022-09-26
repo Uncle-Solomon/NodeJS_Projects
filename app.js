86b8052fc603460613b7cc1849f64a3144ece08c
Uncle-Solomon <106064395+Uncle-Solomon@users.noreply.github.com>
2022-09-26 12:49:16 +0100

    Connected to mongo atlas and used .env

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const connectDB = require("./db/connect");
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+require('dotenv').config();
 
 // middleware
+
 app.use(express.json());
 // routes
 app.get('/hello', (req, res) => {
@@ -20,4 +23,14 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening at port ${port}...`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening at port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
--- db/connect.js
+++ db/connect.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://Uncle-Solomon:<password>@nodeprojects.k0twnwe.mongodb.net/?retryWrites=true&w=majority;"
+const connectDB = (url) => {
+    return mongoose.connect(url);
+}
 
+module.exports = connectDB;
